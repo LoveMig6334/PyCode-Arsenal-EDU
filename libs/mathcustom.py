a1d4b068fcd87bc1dfc6215f7a5ac858715a52b1
Thatt <tom.tom.thanet@gmail.com>
2025-10-22 12:24:18 +0700

    add a prime factorize function

--- libs/mathcustom.py
+++ libs/mathcustom.py
@@ -21,3 +21,24 @@ class MathDCustom:
             i += 6
 
         return True
+
+    @staticmethod
+    def prime_factorize(num) -> list:
+        """
+        A Function to return the prime factorization of a given number.
+        """
+
+        factors = []
+        while num % 2 == 0:
+            factors.append(2)
+            num //= 2
+
+        for i in range(3, isqrt(num) + 1, 2):
+            while num % i == 0:
+                factors.append(i)
+                num //= i
+
+        if num > 2:
+            factors.append(num)
+
+        return factors
